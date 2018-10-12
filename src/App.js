c2cbd3d49e83c17ee954d55cdfa0465e608533c6
skylucycards <skylucycards@gmail.com>
2018-10-12 14:00:40 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Sky Lucy
           </a>
         </header>
       </div>
